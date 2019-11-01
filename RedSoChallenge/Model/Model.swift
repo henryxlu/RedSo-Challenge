a4421191d002d3ec37822e769345af06c6c7fb37
henryLu <henryxlu220@gmail.com>
2019-11-01 14:45:22 +0800

    model建立

---
+++ RedSoChallenge/Model/Model.swift
@@ -0,0 +1,25 @@
+//
+//  Model.swift
+//  RedSoChallenge
+//
+//  Created by Henry on 2019/11/1.
+//  Copyright © 2019 Henry. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Staff: Codable {
+    let results: [Result]
+    
+    struct Result: Codable {
+        let id: String?
+        let type: String
+        let name, position: String?
+        let expertise: [String]?
+        let avatar: String?
+        let url: String?
+    }
+}
+
