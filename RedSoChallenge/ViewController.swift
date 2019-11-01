2a093900ea3fe87b2520f5162082a39aae40e7d8
henryLu <henryxlu220@gmail.com>
2019-11-01 14:38:11 +0800

    Initial Commit

---
+++ RedSoChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedSoChallenge
+//
+//  Created by Henry on 2019/11/1.
+//  Copyright © 2019 Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
